6026ad8417aa1372a139caa9e30e591492f12d5b
Alec Larsen <hello@alec.ninja>
2019-12-10 21:49:12 -0600

    Simplify

--- functions/badge/badge.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-exports.handler = async function(event, context) {
-	return {
-		statusCode: 200,
-		body: "Hi, mom!",
-	};
-};
---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+exports.handler = async event => {
+  const subject = event.queryStringParameters.name || 'World';
+
+  return {
+    statusCode: 200,
+    body: `Hello ${subject}!`,
+  };
+};
